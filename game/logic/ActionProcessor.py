f1db2385cd4748080aa4a99daa8153135b845dfa
unknown <l.pelcner@lancaster.ac.uk>
2021-02-20 20:16:42 +0100

    WIP: Environmental Manager ActionProcessor

---
+++ game/logic/ActionProcessor.py
@@ -0,0 +1,7 @@
+from ai.Agent import Agent
+
+
+class ActionProcessor:
+
+    def __init__(self, all_agents ):
+        self.all_agents = all_agents
---
+++ game/logic/EnvironmentalManager.py
@@ -0,0 +1,5 @@
+import scipy
+class EnvironmentalManager:
+    def __init__(self):
+        self.all_pollinators= []
+    distance.euclidia
