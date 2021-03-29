3f458ce48eb2216e1cdb1bad63ff25b5720baefe
unknown <l.pelcner@lancaster.ac.uk>
2021-03-30 01:16:15 +0200

    Euclidian distance

--- game/logic/EnvironmentalManager.py
+++ game/logic/EnvironmentalManager.py
@@ -1,7 +1,7 @@
 import scipy
 import numpy as np
 from game.logic import PolinattorsProcessor
-
+import math
 
 class EnvironmentalManager:
     def __init__(self, pollinators_processor: PolinattorsProcessor):
@@ -11,3 +11,21 @@ class EnvironmentalManager:
 
     def calculate_euclidian_distance(self, a, b):
         return np.linalg.norm(a-b)
+
+    def find_closest_pollinator_to_land(self):
+
+        s = [(1, 4), (4, 2), (6, 3)]
+        p = (3, 7)
+
+        p0, p1 = p
+        dist = []
+
+        for s0, s1 in s:
+            dist_ = math.sqrt((p0 - s0) ** 2 + (p1 - s1) ** 2)
+            dist_ = dist_ + 1
+
+            dist.append(dist_)  # S
+    def process_declared_lands(self):
+        for land in self.pollinators_processor.buffer_lands:
+            pass
+        self.pollinators_processor.buffer_lands = []
